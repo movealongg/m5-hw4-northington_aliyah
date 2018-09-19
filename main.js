eb8748802fb4c668c86bc7b98641a5cadfbc8a19
Aliyah Northington <alierin@Aliyahs-MacBook-Pro.local>
2018-09-19 08:14:53 -0500

    Create and log variables of numbers
    This commit logs a random number to the console. Then it logs two
    numbers added together. Lastly, this commit will concatenate two
    random numbers and log it to the console.

---
+++ main.js
@@ -0,0 +1,9 @@
+var num = 21;
+console.log(num);
+
+var numAddition = 2 + 4;
+console.log(numAddition);
+
+var numConcatOne = 93;
+var numConcatTwo = ("7");
+console.log(numConcatOne + numConcatTwo);
